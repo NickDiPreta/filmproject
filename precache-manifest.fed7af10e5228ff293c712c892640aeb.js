10220ac7aa2c35c16eedb1da2de038f3428518a8
nick <nicholasdipreta2020@gmail.com>
2020-06-16 16:43:00 -0400

    Updates

---
+++ precache-manifest.fed7af10e5228ff293c712c892640aeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05c669353c780bbd5aef06c13a2b25c0",
+    "url": "/filmproject/index.html"
+  },
+  {
+    "revision": "34b61ae4524533801cb2",
+    "url": "/filmproject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "26c839d26fc6b01d59f1",
+    "url": "/filmproject/static/js/2.badde1f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/filmproject/static/js/2.badde1f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b61ae4524533801cb2",
+    "url": "/filmproject/static/js/main.7c51a853.chunk.js"
+  },
+  {
+    "revision": "cdf96dc57b33f7d96989",
+    "url": "/filmproject/static/js/runtime-main.1cbc2c7f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/filmproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
